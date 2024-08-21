f39c7925b7c0ee6c61de75e683306afb65eb8f84
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2024-08-20 20:57:47 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Transactional Machine Learning Solution Studio'
+copyright = '2024, Otics Advanced Analytics'
+author = 'Sebastian Maurice'
 
 release = '0.1'
 version = '0.1.0'
